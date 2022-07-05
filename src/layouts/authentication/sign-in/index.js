da74af06dbab0c3153ccb191fbc3609e919bb827
yogyaakalra <yogyaalra01@gmail.com>
2022-07-05 19:05:24 +0530

    First commit Message

---
+++ src/layouts/authentication/sign-in/index.js
@@ -0,0 +1,124 @@
+/*!
+
+=========================================================
+* Vision UI Free React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vision-ui-free-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
+* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-react/blob/master LICENSE.md)
+
+* Design and Coded by Simmmple & Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// Vision UI Dashboard React components
+import VuiBox from "components/VuiBox";
+import VuiTypography from "components/VuiTypography";
+import VuiInput from "components/VuiInput";
+import VuiButton from "components/VuiButton";
+import VuiSwitch from "components/VuiSwitch";
+import GradientBorder from "examples/GradientBorder";
+
+// Vision UI Dashboard assets
+import radialGradient from "assets/theme/functions/radialGradient";
+import palette from "assets/theme/base/colors";
+import borders from "assets/theme/base/borders";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import bgSignIn from "assets/images/signInImage.png";
+
+function SignIn() {
+  const [rememberMe, setRememberMe] = useState(true);
+
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+  return (
+    <CoverLayout
+      title="Welcome Back!"
+      color="white"
+      description="Enter your username and password to sign in"
+      premotto="WE AIM TO SUSTAIN!"
+      motto="CSD IGDTUW"
+      image={bgSignIn}
+    >
+      <VuiBox component="form" role="form">
+        <VuiBox mb={2}>
+          <VuiBox mb={1} ml={0.5}>
+            <VuiTypography component="label" variant="button" color="white" fontWeight="medium">
+              Username
+            </VuiTypography>
+          </VuiBox>
+          <GradientBorder
+            minWidth="100%"
+            padding="1px"
+            borderRadius={borders.borderRadius.lg}
+            backgroundImage={radialGradient(
+              palette.gradients.borderLight.main,
+              palette.gradients.borderLight.state,
+              palette.gradients.borderLight.angle
+            )}
+          >
+            <VuiInput type="usernme" placeholder="Your username..." fontWeight="500" />
+          </GradientBorder>
+        </VuiBox>
+        <VuiBox mb={2}>
+          <VuiBox mb={1} ml={0.5}>
+            <VuiTypography component="label" variant="button" color="white" fontWeight="medium">
+              Password
+            </VuiTypography>
+          </VuiBox>
+          <GradientBorder
+            minWidth="100%"
+            borderRadius={borders.borderRadius.lg}
+            padding="1px"
+            backgroundImage={radialGradient(
+              palette.gradients.borderLight.main,
+              palette.gradients.borderLight.state,
+              palette.gradients.borderLight.angle
+            )}
+          >
+            <VuiInput
+              type="password"
+              placeholder="Your password..."
+              sx={({ typography: { size } }) => ({
+                fontSize: size.sm,
+              })}
+            />
+          </GradientBorder>
+        </VuiBox>
+        <VuiBox display="flex" alignItems="center">
+          <VuiSwitch color="info" checked={rememberMe} onChange={handleSetRememberMe} />
+          <VuiTypography
+            variant="caption"
+            color="white"
+            fontWeight="medium"
+            onClick={handleSetRememberMe}
+            sx={{ cursor: "pointer", userSelect: "none" }}
+          >
+            &nbsp;&nbsp;&nbsp;&nbsp;Remember me
+          </VuiTypography>
+        </VuiBox>
+        <VuiBox mt={4} mb={1}>
+          <VuiButton color="info" fullWidth>
+            SIGN IN
+          </VuiButton>
+        </VuiBox>
+      </VuiBox>
+    </CoverLayout>
+  );
+}
+
+export default SignIn;
